1b75f7c9ce339d8ab72494630c58559117ba7bc4
gz-freedom <myec2005@163.com>
2019-02-14 12:08:43 +0800

    happy valentine's day

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ReadService } from "../read.service";
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
@@ -7,13 +8,33 @@ import { ReadService } from "../read.service";
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  allArticles: any;
   articles: any;
+
+  searchGroup = new FormGroup({
+    searchStr: new FormControl()
+  });
+
   constructor(private rs: ReadService) { }
 
   ngOnInit() {
     this.rs.getArticles().subscribe(res => {
-      this.articles = res;
+      this.articles = this.allArticles = res;
     })
   }
 
+  filterArticles(type) {
+    if(type === "unread") {
+      this.articles = this.allArticles.filter((data) => {
+        return !data.read;
+      });
+    } else if(type === "all") {
+      this.articles = this.allArticles;
+    } else if(type === "search") {
+      let keyword = this.searchGroup.get("searchStr").value.toLowerCase();
+      this.articles = this.allArticles.filter((data) => {
+        return data.title.toLowerCase().indexOf(keyword) !== -1 || data.tags.indexOf(keyword) !== -1;
+      });
+    }
+  }
 }
